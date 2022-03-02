2419b7fd49d461524df81464669e82dde061eb40
“ikarabag1” <irmakkarabag@gmail.com>
2022-03-02 03:52:59 +0300

    MVP and ERD updated, models updated:join table added for user and player

---
+++ migrations/20220301234211-create-player.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.NUMBER
+      },
+      height: {
+        type: Sequelize.NUMBER
+      },
+      weight: {
+        type: Sequelize.NUMBER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('players');
+  }
+};
---
+++ models/player.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class player extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  player.init({
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    age: DataTypes.NUMBER,
+    height: DataTypes.NUMBER,
+    weight: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'player',
+  });
+  return player;
+};
